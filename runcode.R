73f983418f73cd3c89f4a1379914d89ce54a300e
Dai Zhuo Jia <zhuojia.dai@gmail.com>
2013-11-20 03:53:14 +0800

    new

--- runcode.R
+++ runcode.R
@@ -1,15 +1,15 @@
 require(shiny)
-path <- "j:/"
+path <- "j:/intro_to_shiny"
 
 setwd(path)
 # Example 1
-runApp("./0")
+runApp("./0 basic example")
 
 # Example 2 - compute the mean and median
-runApp("./1")
+runApp("./1 mean median")
 
 # Example 3 - compute actionButtonSome
-runApp("./2")
+runApp("./2 isolate")
 
 runApp("./5 progress")
 
